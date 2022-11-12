e17eb8303a85c23e6d2f128597106cc268f73647
Long Tran <longtran@Long-Mac-Pro.local>
2022-11-12 14:05:34 +0700

    chore: formating

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="layout"
-    :class="{ 'sidebar--left': isSidebarOpen, 'sidebar--right': checkoutCartOpen }"
-  >
+  <div class="layout" :class="{ 'sidebar--left': isSidebarOpen, 'sidebar--right': checkoutCartOpen }">
     <VSidebar />
 
     <div class="content-wrapper">
@@ -30,98 +27,84 @@
 </template>
 
 <script>
-import { useDomains } from "@/logic";
-import {
-  useCurrency,
-  useNavigation,
-  useSessionContext,
-  useSharedState,
-  useUIState,
-} from "@shopware-pwa/composables";
-import { ref, defineComponent, provide, watch } from "@vue/composition-api";
-import { getApplicationContext } from "@shopware-pwa/composables";
+import { useDomains } from "@/logic"
+import { useCurrency, useNavigation, useSessionContext, useSharedState, useUIState } from "@shopware-pwa/composables"
+import { ref, defineComponent, provide, watch } from "@vue/composition-api"
+import { getApplicationContext } from "@shopware-pwa/composables"
 
 export default defineComponent({
   name: "DefaultLayout",
   setup() {
-    const { preloadRef } = useSharedState();
-    const { currentDomainId } = useDomains();
-    const { apiInstance } = getApplicationContext({ contextName: "sellerDetail" });
-    const { availableCurrencies, loadAvailableCurrencies } = useCurrency();
+    const { preloadRef } = useSharedState()
+    const { currentDomainId } = useDomains()
+    const { apiInstance } = getApplicationContext({ contextName: "sellerDetail" })
+    const { availableCurrencies, loadAvailableCurrencies } = useCurrency()
 
-    const {
-      setCurrency: setSessionCurrency,
-      currency: currentCurrency,
-    } = useSessionContext();
+    const { setCurrency: setSessionCurrency, currency: currentCurrency } = useSessionContext()
 
-    const { loadNavigationElements, navigationElements } = useNavigation();
-    const {
-      loadNavigationElements: loadService,
-      navigationElements: serviceElements,
-    } = useNavigation({
+    const { loadNavigationElements, navigationElements } = useNavigation()
+    const { loadNavigationElements: loadService, navigationElements: serviceElements } = useNavigation({
       type: "service-navigation",
-    });
+    })
 
-    preloadRef(navigationElements, () => loadNavigationElements({ depth: 1 }));
-    preloadRef(serviceElements, () => loadService({ depth: 2 }));
-    loadAvailableCurrencies();
+    preloadRef(navigationElements, () => loadNavigationElements({ depth: 1 }))
+    preloadRef(serviceElements, () => loadService({ depth: 2 }))
+    loadAvailableCurrencies()
 
     const { isOpen: checkoutCartOpen } = useUIState({
       stateName: "CART_SIDEBAR_STATE",
-    });
+    })
 
-    const isSidebarOpen = ref(false);
+    const isSidebarOpen = ref(false)
     const toggleSidebar = ($x, ...$rest) => {
-      isSidebarOpen.value = !isSidebarOpen.value;
-    };
+      isSidebarOpen.value = !isSidebarOpen.value
+    }
 
-    const loading = ref(false);
+    const loading = ref(false)
 
     const loadSellerDetail = async (params) => {
-      loading.value = true;
-      const { data } = await apiInstance.invoke.get(`/store-api/seller/${params.id}`);
-      seller.value = data;
-      loading.value = false;
-    };
+      loading.value = true
+      const { data } = await apiInstance.invoke.get(`/store-api/seller/${params.id}`)
+      seller.value = data
+      loading.value = false
+    }
 
-    const seller = ref({});
+    const seller = ref({})
     const setSeller = (data) => {
-      data.id && loadSellerDetail(data);
-    };
+      data.id && loadSellerDetail(data)
+    }
 
-    provide("isSidebarOpen", isSidebarOpen);
-    provide("toggleSidebar", toggleSidebar);
-    provide("seller", { setSeller, seller });
+    provide("isSidebarOpen", isSidebarOpen)
+    provide("toggleSidebar", toggleSidebar)
+    provide("seller", { setSeller, seller })
 
     watch(currentDomainId, () => {
-      loadNavigationElements({ depth: 1 });
-      loadService({ depth: 2 });
-    });
+      loadNavigationElements({ depth: 1 })
+      loadService({ depth: 2 })
+    })
 
     const setDefaultCurrency = () => {
-      const defaultCurrency = availableCurrencies.value.find(
-        (entry) => entry.isSystemDefault
-      );
+      const defaultCurrency = availableCurrencies.value.find((entry) => entry.isSystemDefault)
 
       if (defaultCurrency && currentCurrency.value?.id !== defaultCurrency.id) {
-        setSessionCurrency(defaultCurrency);
+        setSessionCurrency(defaultCurrency)
       }
-    };
+    }
 
     watch(currentCurrency, () => {
       if (currentCurrency.value?.isSystemDefault) {
-        return;
+        return
       }
-      setDefaultCurrency();
-    });
+      setDefaultCurrency()
+    })
 
     return {
       checkoutCartOpen,
       isSidebarOpen,
       seller,
       loading,
-    };
+    }
   },
   middleware: ["pages"],
-});
+})
 </script>
