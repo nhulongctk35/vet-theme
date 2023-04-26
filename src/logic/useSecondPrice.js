39a98c313bcdfdb015e631dbcdfd1a1c0eb22d6c
Long Tran <longntran48@gmail.com>
2023-04-26 23:14:00 +0700

    fix: correct getting current locale in useSecondPrice

--- src/logic/useSecondPrice.js
+++ src/logic/useSecondPrice.js
@@ -1,16 +1,15 @@
 import { computed } from "@vue/composition-api"
-import { useCurrency } from "@shopware-pwa/composables"
-import { getApplicationContext } from "@shopware-pwa/composables"
+import { useCurrency, useSharedState } from "@shopware-pwa/composables"
 import { formatPrice } from "@/helpers/formatPrice"
 
 export function useSecondPrice() {
+  const { sharedRef } = useSharedState()
   const { availableCurrencies } = useCurrency()
-  const { route } = getApplicationContext({ contextName: "useSecondPrice" })
-
-  const secondCurrency = availableCurrencies.value.find((entry) => entry.id === route?.meta.currencyId)
+  const currentDomain = sharedRef("sw-current-domain")
+  const secondCurrency = availableCurrencies.value.find((entry) => entry.id === currentDomain.value?.currencyId)
 
   const show = computed(() => {
-    return ["da-DK"].includes(route?.meta?.languageLocaleCode)
+    return ["da-DK"].includes(currentDomain.value?.languageLocaleCode)
   })
 
   const secondPrice = (price) => formatPrice(secondCurrency?.factor * price, { symbol: secondCurrency?.symbol })
